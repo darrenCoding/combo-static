c11cfc1284f0da27fa6ab15032c9878efc87f3c6
darrenCoding <linfang92926@gmail.com>
2016-01-22 20:25:07 +0800

    fix a bug

--- config/local.js
+++ config/local.js
@@ -4,7 +4,7 @@
 //本地环境配置
 module.exports = {
 	"base_path" : "/Users/linfang/Documents/leju/leju-combo/", //文件存放路径
-	"combo_dir" : ";", //跨目录文件分隔符
+	"combo_dir" : "??", //跨目录文件分隔符
 	"combo_file" : ",", //同目录下文件分隔符
 	"combo_format" : ["js","css"], //支持合并功能的文件后缀名
 	"js_module" : {
