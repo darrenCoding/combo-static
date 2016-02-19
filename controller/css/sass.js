b9ba2bb6ad51b47a6a67352089f4f1d067b3d9c9
darrenCoding <linfang92926@gmail.com>
2016-02-19 20:52:12 +0800

    fix a bug

--- controller/css/sass.js
+++ controller/css/sass.js
@@ -7,16 +7,19 @@ const event = require('../../lib/util').event;
 const log4js = require('../../config/log');
 
 class Sass{
-	constructor(data,suffix,fn){
+	constructor(data,suffix,fn,fileArr){
 		this.fn = fn;
+		this.fileArr = fileArr;
 		this.suffix = suffix;
 		this.handleJs(data);
 	}
 
 	handleJs(data){
 		sass.render({
-		  	data : data
+		  	data : data,
+		  	includePaths : ["/Users/linfang/Documents/leju/leju-combo/asset/css"]
 		}, (err, result) => {
+			this.fileArr.push.apply(this.fileArr,result.stats.includedFiles);
 			if(!err){
 				if(Buffer.isBuffer(result.css)){
 					event.emit("compileData",this.fn,this.suffix,iconv.decode(result.css,'utf8'))
